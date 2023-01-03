312a6f28a8e5f0ad19dc201643019bdcd07de7fe
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-02 16:55:16 -0800

    Create Task.js

---
+++ src/components/Task.js
@@ -0,0 +1,8 @@
+export default function Task() {
+  return (
+    <div className="task">
+      <p className="task--title">Build UI for onboarding flow</p>
+      <p className="num-of-subtasks">{`0 of 3 subtasks`}</p>
+    </div>
+  );
+}
