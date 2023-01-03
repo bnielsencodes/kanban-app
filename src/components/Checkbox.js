e9fd49b259ccada39fa00893b6cc3133468780fb
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-02 16:58:16 -0800

    Add onClick function to toggle className of "checked"

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -3,9 +3,15 @@ import { useState } from "react";
 export default function Checkbox() {
   const [checked, setChecked] = useState(false);
 
+  function toggleChecked() {
+    setChecked(!checked);
+  }
+
   return (
     <div className="checkbox">
       <input
+        className={checked ? "checked" : null}
+        onClick={toggleChecked}
         type="checkbox"
         name=""
         id="checkbox-id"
