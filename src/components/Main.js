d073de07bfb68613dadfdaca46b63422c8fce42a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-02 16:50:44 -0800

    Import and add react-indiana-drag-scroll

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,19 @@
+import ScrollContainer from "react-indiana-drag-scroll";
+import "react-indiana-drag-scroll/dist/style.css";
+
 export default function Main() {
   return (
     <div className="main">
+      <ScrollContainer
+        className="scroll-container"
+        nativeMobileScroll={true}
+        vertical={false}
+        hideScrollbars={true}
+        mouseScroll={{
+          inertia: false,
+        }}
+      >
+      </ScrollContainer>
     </div>
   );
 }
