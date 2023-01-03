31f980d02d8151c674a1e475e75216ab8e3fc1f1
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-02 17:01:23 -0800

    Add title and description

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -1,6 +1,20 @@
+import ellipsisIcon from "../assets/icon-vertical-ellipsis.svg";
+
 export default function TaskModal() {
   return (
     <div className="task-modal">
+      <div className="task-modal--title-container">
+        <p className="task-modal--title">
+          Research pricing points of various competitors and trial different
+          business models
+        </p>
+        <img src={ellipsisIcon} alt="veritcal ellipsis icon" />
+      </div>
+      <p className="task--description">
+        We know what we're planning to build for version one. Now we need to
+        finalize the first pricing model we'll use. Keep iterating the subtasks
+        until we have a coherent proposition.
+      </p>
     </div>
   );
 }
